a6a2a2644cae8e72c6758b7bb1348c82c5a84903
“Utkal9” <utkalbehera59@gmail.com>
2025-10-25 23:10:41 +0530

    Depolying frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-    ? "https://apnacollegebackend.onrender.com"
+    ? "https://apnavideocallbackend-0wh0.onrender.com"
     : "http://localhost:8000";
 
 export default server;
